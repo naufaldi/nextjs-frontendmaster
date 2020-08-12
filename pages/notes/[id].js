b15d941a3c468d4035406fb70743ec0908c82e84
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-08-12 21:27:49 +0700

    ad component, nav

---
+++ pages/notes/[id].js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+const Page = () => {
+  const router = useRouter();
+  const { id } = router.query
+  return (
+    <div>
+      <h1> Note { id } </h1>
+    </div>
+  )
+}
+
+export default Page
---
+++ pages/notes/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <div>
+      Notes Index
+    </div>
+  )
+}
+
+export default Pages
