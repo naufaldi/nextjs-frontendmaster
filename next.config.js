586f58e914f7850f1c81c61d7b435bea134979ac
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-08-25 17:21:21 +0700

    add api handler

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-const  { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
-// Setting up module for server and production
-module.exports = (phase, {defaultConfig})=>{
-  if(phase === PHASE_DEVELOPMENT_SERVER){
-    console.log("Server")
-    return defaultConfig
-  }
-  return defaultConfig
- 
-}
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+// load env in folder
+dotenvLoad()
+const withEnv = nextEnv();
+
+module.exports = withEnv()
