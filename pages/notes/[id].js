610bd7b5b5b29479eae615e0f51ef52ebefb29cb
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-09-01 02:37:56 +0700

    get server side

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -3,13 +3,28 @@ import { jsx } from 'theme-ui'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-export default () => {
-  const router = useRouter()
-  const { id }= router.query
+export default ({notes}) => {
 
   return (
     <div sx={{variant: 'containers.page'}}>
-      <h1>Note: {id} </h1>
+      <h1>Note: {notes.title} </h1>
     </div>
   )
+}
+
+export async function getServerSideProps({params,req,res}){
+  const response = await fetch(`hhtp://localhost:3000/api/note/${params.id}`)
+  if(!response.ok){
+    res.writeHead(302,{
+      Location: 'notes'
+    })
+    res.end()
+    return {
+      props: {}
+    }
+  }
+  const { data } = await response.json()
+  return {
+    props: { note : data }
+  }
 }
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -25,3 +25,12 @@ export default () => {
     </div>
   )
 }
+
+export default function getServerStaticProps(){
+  const res = await fetch('http://localhost:3000/api/note/ ')
+  const { data }= await res.json()
+  console.log(data)
+  return {
+   props: { notes : data } 
+  }
+}
